8061f8502ace856885274ed30766e983a33d90d7
Vadym Liesin <liesinvadym@gmail.com>
2019-10-30 21:32:47 +0200

    Deploy the project to angular, create layout
    Create home component. Add routing and connected modules. Create and import base scss files.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { LayoutModule } from './shared/layout/layout.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    LayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
