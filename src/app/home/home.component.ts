a4c00018d178a88115d1796c72d3e7a4a72979e8
Vadym Liesin <liesinvadym@gmail.com>
2019-11-02 00:24:20 +0200

    Include ngx slick carousel for home page
    fix styles for header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  slides = [
+    { img: "assets/images/home/home-slider/home-slide1.jpg" },
+    { img: "http://placehold.it/350x150/111111" },
+    { img: "http://placehold.it/350x150/333333" },
+    { img: "http://placehold.it/350x150/666666" }
+  ];
+  slideConfig = {
+    nextArrow: ''
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
