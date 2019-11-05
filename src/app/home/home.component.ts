32a55489f16cbb384700e20adc30f031e472cf4c
Vadym Liesin <liesinvadym@gmail.com>
2019-11-05 18:34:44 +0200

    (Page making): footer
    Add new section for arrivals, add dress images and social icons, fix some styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
   slides = [
@@ -12,11 +12,85 @@ export class HomeComponent implements OnInit {
     { img: "http://placehold.it/350x150/333333" },
     { img: "http://placehold.it/350x150/666666" }
   ];
+
   slideConfig = {
-    nextArrow: ''
+    dots: true
   };
+
+  public showArrivals: any;
+  public showedAll: boolean = false;
+
+  public arrivals: any = [
+    {
+      img: "assets/images/dress-colection/dress1.jpg",
+      name: "Straight Leg Jeans",
+      price: "55.00"
+    },
+    {
+      img: "assets/images/dress-colection/dress2.jpg",
+      name: "Boyfriend T-Shirt with Bohemian Print",
+      price: "34.25"
+    },
+    {
+      img: "assets/images/dress-colection/dress3.jpg",
+      name: "Only Busted Knee Jean",
+      price: "140.50"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    },
+    {
+      img: "assets/images/dress-colection/dress4.jpg",
+      name: "Only Skinny Jeans",
+      price: "12.75"
+    }
+  ];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.showArrivals = this.arrivals.slice(0, 4);
+  }
+
+  public showAll(): void {
+    this.showArrivals = this.arrivals;
+    this.showedAll = true;
+  }
 }
